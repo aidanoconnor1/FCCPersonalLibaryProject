3fc807c330c0362fa86921fc696a4ad294b2ef12
aoc <aidanoconnor1@live.co.uk>
2020-03-19 19:04:03 +0000

    repolinkadded

--- PersonalLibary/client/src/Components/NavBar.js
+++ PersonalLibary/client/src/Components/NavBar.js
@@ -9,7 +9,7 @@ export const NavBar = () => (
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/404">Github Repo</Nav.Link>
+            <Nav.Link href="https://github.com/aidanoconnor1/FCCPersonalLibaryProject">Github Repo</Nav.Link>
             <Nav.Link href="https://www.freecodecamp.org/learn/information-security-and-quality-assurance/information-security-and-quality-assurance-projects/personal-library">About</Nav.Link>
             </Nav>
         </Navbar.Collapse>
